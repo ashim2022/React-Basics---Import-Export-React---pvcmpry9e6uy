c6efbe860de74079a7c7e69e1321b80d7c47694b
Ashim Shaw <shawashim72@gmail.com>
2022-07-05 22:16:31 +0530

    starter files

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import Heading from "./Heading";
+import InputQuery from "./InputQuery";
+import SubmitButton from "./SubHeading";
+import SubHeading from "./SubmitButton";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading/>
+      <InputQuery/>
+      <SubmitButton/>
+      <SubHeading/>
     </div>
   )
 }
+export default App;
 
 
